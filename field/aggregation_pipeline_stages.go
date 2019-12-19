c4bed3b33edb9c341b814faa0508aee1f67923df
Mehran Prs <mehr.prs@gmail.com>
2019-12-20 00:09:32 +0330

    Add all pipeline stage fields

---
+++ field/aggregation_pipeline_stages.go
@@ -0,0 +1,112 @@
+package field
+
+import "go.mongodb.org/mongo-driver/bson"
+
+// $bucket fields
+const (
+	GroupBy    = "groupBy"
+	Boundaries = "boundaries"
+	Default    = "default"
+	Output     = "output"
+)
+
+// $bucketAuto
+const (
+	// GroupBy     = "groupBy" // Declared
+	Buckets = "buckets"
+	// Output      = "output" // Declared
+	Granularity = "granularity"
+)
+
+// $collStats
+const (
+	LatencyStats = "latencyStats"
+	StorageStats = "storageStats"
+	Count        = "count"
+)
+
+// $currentOp
+const (
+	AllUsers        = "allUsers"
+	IdleConnections = "idleConnections"
+	IdleCursors     = "idleCursors"
+	IdleSessions    = "idleSessions"
+	LocalOps        = "localOps"
+)
+
+// $geoNear
+const (
+	Near               = "near"
+	DistanceField      = "distanceField"
+	Spherical          = "spherical"
+	MaxDistance        = "maxDistance"
+	Query              = "query"
+	DistanceMultiplier = "distanceMultiplier"
+	IncludeLocs        = "includeLocs"
+	UniqueDocs         = "uniqueDocs"
+	MinDistance        = "minDistance"
+	Key                = "key"
+)
+
+// $graphLookup
+const (
+	From                    = "from"
+	StartWith               = "startWith"
+	ConnectFromField        = "connectFromField"
+	ConnectToField          = "connectToField"
+	As                      = "as"
+	MaxDepth                = "maxDepth"
+	DepthField              = "depthField"
+	RestrictSearchWithMatch = "restrictSearchWithMatch"
+)
+
+// $group
+const (
+// Id="_id" // Declared
+)
+
+// $listLocalSessions
+const (
+// AllUsers = "allUsers" // Declared
+)
+
+var (
+	EmptyDoc    = bson.M{}
+	AllUsersDoc = bson.M{AllUsers: true}
+)
+
+// $listSessions : Same as $listLocalSessions.
+
+// $lookup fields
+const (
+	// From         = "from" // Declared
+	LocalField   = "localField"
+	ForeignField = "foreignField"
+	// As           = "as" // Declared
+)
+
+// $merge
+const (
+	Into           = "into"
+	On             = "on"
+	Let            = "let"
+	WhenMatched    = "whenMatched"
+	WhenNotMatched = "whenNotMatched"
+)
+
+// $replaceRoot
+const (
+	NewRoot = "newRoot"
+)
+
+// $sample
+const (
+	Size = "size"
+)
+
+// $unwind
+const (
+	Path                       = "path"
+	IncludeArrayIndex          = "includeArrayIndex"
+	PreserveNullAndEmptyArrays = "preserveNullAndEmptyArrays"
+)
--- field/base.go
+++ field/base.go
@@ -1,3 +1,13 @@
 package field
 
+import "go.mongodb.org/mongo-driver/bson"
+
 const Id = "_id"
+
+var Empty = bson.M{}
+
+// TODO: Extract all field names from :
+// cont.todo: https://docs.mongodb.com/manual/reference/operator/query/
+// cont.todo: https://docs.mongodb.com/manual/reference/operator/update/
+// cont.todo: https://docs.mongodb.com/manual/reference/operator/aggregation/
+// cont.todo: https://docs.mongodb.com/manual/reference/operator/query-modifier/
