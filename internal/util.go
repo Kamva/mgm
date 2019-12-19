fac04b528c6e0fece6b35bbc1c6f83a7d3d685f6
Mehran Prs <mehr.prs@gmail.com>
2019-12-20 01:45:48 +0330

    Add InterfaceIsNil function

--- internal/util.go
+++ internal/util.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"reflect"
 	"regexp"
 	"strings"
 )
@@ -12,4 +13,31 @@ func ToSnakeCase(str string) string {
 	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
 	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
 	return strings.ToLower(snake)
+}
+
+func PanicErr(err error) {
+	if err != nil && !reflect.ValueOf(err).IsNil() {
+		panic(err)
+	}
+}
+
+func InterfaceIsNil(val interface{}) bool {
+	// We can not directly check interface is nil or
+	// not, check this https://play.golang.org/p/Isoo0CcAvr.
+	// Also we firstly check `val==nil` because reflection
+	// can not get value of zero val.
+	return val == nil || reflect.ValueOf(val).IsNil()
+}
+
+// AnyNil return true if exists any nil value in passed params.
+func AnyNil(values ...interface{}) bool {
+	for _, val := range values {
+
+		if InterfaceIsNil(val) {
+			return true
+		}
+
+	}
+
+	return false
 }
