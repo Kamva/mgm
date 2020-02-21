023fefb3ef2b3440223a6eb43c09652493bb6f65
Mehran Prs <mehr.prs@gmail.com>
2020-02-21 16:08:30 +0330

    Change TransactionWithClient signature.

--- transaction.go
+++ transaction.go
@@ -10,16 +10,16 @@ type TransactionFunc func(session mongo.Session, sc mongo.SessionContext) error
 
 // Transaction run a transaction with default client..
 func Transaction(f TransactionFunc) error {
-	return TransactionWithClient(client, ctx(), f)
+	return TransactionWithClient(ctx(), client, f)
 }
 
 // TransactionWithCtx run transaction with the given context and default client.
 func TransactionWithCtx(ctx context.Context, f TransactionFunc) error {
-	return TransactionWithClient(client, ctx, f)
+	return TransactionWithClient(ctx, client, f)
 }
 
 // TransactionWithClient run transaction with the given client.
-func TransactionWithClient(client *mongo.Client, ctx context.Context, f TransactionFunc) error {
+func TransactionWithClient(ctx context.Context, client *mongo.Client, f TransactionFunc) error {
 	session, err := client.StartSession() //start session need to get options.
 	if err != nil {
 		return err
