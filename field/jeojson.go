d6e646321e5f5bd1d4c6798d5c4dcc827a97ee1f
Mehran Prs <mehr.prs@gmail.com>
2020-04-12 12:25:38 +0430

    Add JeoJSON fields

---
+++ field/jeojson.go
@@ -0,0 +1,12 @@
+package field
+
+// JeoJSON reference: https://docs.mongodb.com/manual/reference/geojson/
+const (
+	Point              = "Point"
+	LineString         = "LineString"
+	Polygon            = "Polygon"
+	MultiPoint         = "MultiPoint"
+	MultiLineString    = "MultiLineString"
+	MultiPolygon       = "MultiPolygon"
+	GeometryCollection = "GeometryCollection"
+)
